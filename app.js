77e542891e45597754940abd9f3721a22fd5be3e
Ines-Abdelaziz <ji_abdelaziz@esi.dz>
2024-02-15 15:44:50 +0100

    allow cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 
 // Middleware
 app.use(express.json());
-
+app.use(cors());
 // Routes
 app.use('/', routes);
 
