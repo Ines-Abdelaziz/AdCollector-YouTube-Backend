010714fa474c13b4ce4fd9f506e0409ab16622f8
Ines-Abdelaziz <ji_abdelaziz@esi.dz>
2024-02-23 14:07:24 +0100

    change bycrypt package to argon2

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,17 +1,13 @@
 const AdminModel = require('../models/adminModel');
-const crypto = require('crypto'); // Import the crypto module
-
+const argon2 = require('argon2'); // Import argon2 library
+//
 const jwt = require('jsonwebtoken');
 
 class AdminController {
     static async registerAdmin(req, res) {
         const { username, password } = req.body;
         try {
-            // Generate a salt
-            const salt = crypto.randomBytes(16).toString('hex');
-            // Hash the password using the salt
-            const hashedPassword = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
-            
+            const hashedPassword = await argon2.hash(password); // Hash password with argon2
             await AdminModel.registerAdmin(username, hashedPassword);
             res.status(201).json({ message: 'Admin registered successfully' });
         } catch (error) {
@@ -26,11 +22,8 @@ class AdminController {
             if (!admin) {
                 return res.status(401).json({ error: 'Invalid username or password' });
             }
-            // Hash the provided password with the same salt used for registration
-            const hashedPassword = crypto.pbkdf2Sync(password, admin.salt, 1000, 64, 'sha512').toString('hex');
-
-            // Compare the hashed passwords
-            if (hashedPassword !== admin.password) {
+            const isPasswordValid = await argon2.verify(admin.password, password); // Verify password with argon2
+            if (!isPasswordValid) {
                 return res.status(401).json({ error: 'Invalid username or password' });
             }
             const token = jwt.sign({ username: admin.username }, process.env.JWT_SECRET);
