6bff492a34fd394ac4877f97b48c65313deadf7b
Ines-Abdelaziz <ji_abdelaziz@esi.dz>
2024-03-05 10:32:05 +0100

    update ad model

--- models/adModel.js
+++ models/adModel.js
@@ -11,9 +11,9 @@ class AdModel {
     }
 
     static async addAd(adData) {
-        const { advertiser, advertiser_location, topic, google_information, other_information ,advertiser_link} = adData;
+        const { adlink,advertiser, advertiser_location, topic, google_information, other_information ,advertiser_link} = adData;
         try {
-            const result = await pool.query('INSERT INTO ads ( advertiser, advertiser_location, topic, google_information, other_information,advertiser_link) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [advertiser, advertiser_location, topic, google_information, other_information,advertiser_link]);
+            const result = await pool.query('INSERT INTO ads ( adlink,advertiser, advertiser_location, topic, google_information, other_information,advertiser_link) VALUES ($1, $2, $3, $4, $5, $6,$7) RETURNING *', [adlink,advertiser, advertiser_location, topic, google_information, other_information,advertiser_link]);
             return result.rows[0];
         } catch (error) {
             throw new Error('Error creating ad');
@@ -31,9 +31,9 @@ class AdModel {
     }
     //check id ad existis by comparing all columns of the ad
     static async getAdByAllColumns(adData) {
-        const { advertiser, advertiser_location, topic, google_information, other_information ,advertiser_link} = adData;
+        const {advertiser, advertiser_location, topic, google_information, other_information ,advertiser_link,adlink} = adData;
         try {
-            const result = await pool.query('SELECT * FROM ads WHERE advertiser = $1 AND advertiser_location = $2 AND topic = $3 AND google_information = $4 AND other_information = $5 AND advertiser_link = $6', [advertiser, advertiser_location, topic, google_information, other_information,advertiser_link]);
+            const result = await pool.query('SELECT * FROM ads WHERE advertiser = $1 AND advertiser_location = $2 AND topic = $3 AND google_information = $4 AND other_information = $5 AND advertiser_link = $6 AND adlink = $7', [advertiser, advertiser_location, topic, google_information, other_information,advertiser_link,adlink]);
             return result.rows[0];
         } catch (error) {
             console.error('Error fetching ad by ID:', error);
