05ac6e1724853f44f4b33d793ff90bd720e2a7b6
Hiba-7 <jh_louzzani@esi.dz>
2024-07-28 12:53:24 +0200

    added message I changed the backend to verify

--- models/user_ad_videoModel.js
+++ models/user_ad_videoModel.js
@@ -7,8 +7,10 @@ class UserAdVideoModel {
       const result = await pool.query(
         "INSERT INTO user_ad_video ( ad_id, video_id,channel_id,user_id) VALUES ($1, $2, $3, $4) RETURNING *",
         [ad_id, video_id, channel_id, user_id]
+        
       );
-      return result.rows[0];
+      console.log("I changed the backend");
+      return { ...result.rows[0], message: "I changed the backend" };
     } catch (error) {
       console.log(error);
       throw new Error("Error adding user ad video association");
